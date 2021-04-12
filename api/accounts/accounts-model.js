08a533d49219de218adc0cba5bc6690f5eceefe5
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-12 23:55:05 +0100

    accounts-model functions created

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,32 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // `getAll` resolves to an array of accounts (or an empty array)
+  return db('accounts')
 }
 
-const getById = id => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  // `getById` resolves to an account by the given id
+  return db('accounts').where({ id }).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async (account) => {
+  // `create` resolves to the newly created account
+  const [ id ] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  // `updateById` resolves to the updated account
+  await db('accounts').where({ id }).update(account)
+  return getById(id)
 }
 
-const deleteById = id => {
-  // DO YOUR MAGIC
+const deleteById = async (id) => {
+  // `deleteById` resolves to the deleted account
+  const deletedAccount = await getById(id)
+  await db('accounts').where({ id }).delete()
+  return deletedAccount
 }
 
 module.exports = {
