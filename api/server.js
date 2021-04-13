4c9d429a58f984aac24431c58c6dfa4868d169ad
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-13 01:49:08 +0100

    server setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,15 @@
+// Imports
 const express = require("express");
+const accountsRouter = require('./accounts/accounts-router')
 
+// Express server instance
 const server = express();
 
+// Middleware
 server.use(express.json());
 
+// Routers
+server.use('/api/accounts', accountsRouter)
+
+// Exports
 module.exports = server;
