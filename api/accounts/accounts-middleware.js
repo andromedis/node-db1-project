f494a48a5e02294e51f2c3f8c38d1c1f759e5ea3
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-04-14 02:14:44 +0100

    debugging

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,31 +1,37 @@
 const Accounts = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-    const generateMessage = ({ name, budget }) => {
-        if (!name || !budget)
-            return 'name and budget are required'
-        else if (typeof name !== 'string')
-            return 'name of account must be a string'
-        else if (name.trim().length < 3 || name.trim().length > 100)
-            return 'name of account must be between 3 and 100'
-        else if (typeof budget !== 'number')
-            return 'budget of account must be a number'
-        else if (budget < 0 || budget > 1000000)
-            return 'budget of account is too large or too small'
-    }  
-    const message = generateMessage(req.body)
-    if (message)
-        res.status(400).json({ message: message })
-    else
+    const { name, budget } = req.body;
+    if ( name === undefined || budget === undefined ) {
+        res.status(400).json({message: "name and budget are required"})
+    }
+    else if (typeof name !== "string") {
+        res.status(400).json({ message: `name of account must be a string.`})
+    }
+    else if (name.trim().length < 3 || name.trim().length > 100) {
+        res.status(400).json({ message: "name of account must be between 3 and 100"})
+    }
+    else if (typeof budget !== "number") {
+        res.status(400).json({ message: "budget of account must be a number" })
+    }
+    else if (budget < 0 || budget > 1000000) {
+        res.status(400).json({ message: "budget of account is too large or too small" })
+    }
+    else {
+        req.body = { name: name.trim(), budget}
         next()
+    }
 }
 
 exports.checkAccountNameUnique = async (req, res, next) => {
+    const { name } = req.body
+    const { id } = req.params
     try {
         const accounts = await Accounts.getAll()
-        const nameMatches = accounts.filter(account => 
-            account.name === req.body.name.trim()).length
-        if (nameMatches)
+        const matches = accounts.filter(account => 
+            account.name === name && account.id != id 
+        ).length
+        if (matches)
             res.status(400).json({ message: 'that name is taken' })
         else
             next()
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,78 @@
+// Imports
 const router = require('express').Router()
+const Accounts = require('./accounts-model')
+const mw = require('./accounts-middleware')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+
+// Endpoints
+router.get('/', async (req, res, next) => {
+    // `[GET] /api/accounts` returns an array of accounts (or an empty array if there aren't any).
+    try {
+        const accounts = await Accounts.getAll()
+        res.status(200).json(accounts)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', mw.checkAccountId, async (req, res, next) => {
+    // `[GET] /api/accounts/:id` returns an account by the given id.
+    try {
+        const account = await Accounts.getById(req.params.id)
+        res.status(200).json(account)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', 
+    mw.checkAccountPayload, 
+    mw.checkAccountNameUnique, async (req, res, next) => {
+    // `[POST] /api/accounts` returns the created account. Leading or trailing 
+    // whitespace on budget `name` should be trimmed before saving to db.
+        try {
+            const newAccount = await Accounts.create(req.body)
+            res.status(201).json(newAccount)
+        }
+        catch (err) {
+            next(err)
+        }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', 
+    mw.checkAccountId, 
+    mw.checkAccountPayload, 
+    mw.checkAccountNameUnique, async (req, res, next) => {
+    // `[PUT] /api/accounts/:id` returns the updated account. Leading or trailing 
+    // whitespace on budget `name` should be trimmed before saving to db.
+        const { id } = req.params
+        try {
+            const updatedAccount = await Accounts.updateById(id, req.body)
+            res.status(200).json(updatedAccount)
+        }
+        catch (err) {
+            next(err)
+        }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', mw.checkAccountId, async (req, res, next) => {
+    // `[DELETE] /api/accounts/:id` returns the deleted account.
+    try {
+        const deletedAccount = await Accounts.deleteById(req.params.id)
+        res.status(200).json(deletedAccount)
+    }
+    catch (err) {
+        next(err)
+    }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+    // Error handling and response
+    res.status(500).json({ message: err.message, stack: err.stack })
 })
 
+
+// Exports
 module.exports = router;
